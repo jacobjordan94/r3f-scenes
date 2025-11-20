ad6c03f61172ac6d5791917c2edafd647e799773
Jacob Jordan <jacobjordan94@live.com>
2025-11-19 23:42:29 -0600

    add stairs

--- src/App.tsx
+++ src/App.tsx
@@ -137,11 +137,21 @@ const Monolith = ({ number, x, z, rotation, ...props }: MonolithProps) => {
 const Desk = ({...props}) => {
   return (
     <group {...props} position-y={0}>
-      <Gendo position-y={0.624} position-x={-0.35}rotation={[0, -(Math.PI / 2), 0]} />
-      <mesh castShadow position-y={.25} scale={[1, 0.75, 3]}>
+      <Gendo position-y={0.624} position-x={-0.35} rotation={[0, -(Math.PI / 2), 0]} scale={0.25} />
+      <spotLight
+        position={[-0.35, 3, 0]}
+        target-position={[-0.35, 0.624, 0]}
+        angle={Math.PI}
+        penumbra={1}
+        intensity={100}
+        castShadow
+        color={0xffffff}
+      />
+      <mesh castShadow position-y={.25} scale={[1.5, 0.75, 2]}>
         <boxGeometry />
         <meshPhongMaterial color={0x1a1a1a} />
       </mesh>
+      <Stairs position={[1.6, -1.062, 0.9]} scale={0.03} />
     </group>
   );
 };
@@ -184,7 +194,21 @@ const MonolithLight = ({ x, z, rotation }: MonolithLightProps) => {
 
 const Gendo = ({...props}) => {
   const { scene: gendo } = useGLTF('/models/gendo.glb');
-  return <primitive {...props} object={gendo} scale={0.25} />
+  return <primitive {...props} object={gendo} />
+};
+const Stairs = ({...props}) => {
+  const { scene: stairs } = useGLTF('/models/stairs.glb');
+
+  // Apply the same material to all meshes in the model
+  stairs.traverse((child: any) => {
+    if (child.isMesh) {
+      child.material = new THREE.MeshPhongMaterial({ color: 0x1a1a1a });
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  return <primitive {...props} object={stairs} />;
 };
 
 const FlyControls = () => {
